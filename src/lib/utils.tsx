f6426aa3219dbe1ab39a5710c6a0f6bddba0ae7e
Navin-Lakshman-S <navinlakshman2062@gmail.com>
2024-10-06 22:26:21 +0530

    updated

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -4,10 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function formatDate(date: Date) {
-  return new Intl.DateTimeFormat("en-US", {
-    dateStyle: "short",
-    timeStyle: "short",
-  }).format(date);
-}
